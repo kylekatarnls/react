fe78553b2be8c561e2a7aa82db6a1471c6841dfe
Kyle <kylekatarnls@gmail.com>
2016-08-02 17:46:56 +0200

    Try antoher install setting

--- tests/installV8js.php
+++ tests/installV8js.php
@@ -2,7 +2,6 @@
 
 if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
     foreach (array(
-        'apt-get install libv8-dev libv8-dbg g++ cpp',
         'printf "\n" | pecl install -f v8js',
         'echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini',
     ) as $command) {
