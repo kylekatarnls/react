cb330ee3b99a1d98c08c79a1de585ca3b2f866a5
Kyle <kylekatarnls@gmail.com>
2016-08-02 17:18:53 +0200

    Verbose installation

--- tests/installV8js.php
+++ tests/installV8js.php
@@ -1,6 +1,8 @@
 <?php
 
 if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
-    shell_exec('printf "\n" | pecl install -f v8js 2>&1');
-    shell_exec('echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini 2>&1');
+    echo "\nprintf \"\\n\" | pecl install -f v8js\n\n";
+    echo shell_exec('printf "\n" | pecl install -f v8js 2>&1');
+    echo "\necho \"extension=v8js.so\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini\n\n";
+    echo shell_exec('echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini 2>&1');
 }
