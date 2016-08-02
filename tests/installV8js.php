9a61f7649746630b8f678d5c8e30c5758ef1729e
Kyle <kylekatarnls@gmail.com>
2016-08-02 17:34:21 +0200

    Fix install

--- tests/installV8js.php
+++ tests/installV8js.php
@@ -1,8 +1,12 @@
 <?php
 
 if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
-    echo "\nprintf \"\\n\" | pecl install -f v8js\n\n";
-    echo shell_exec('printf "\n" | pecl install -f v8js 2>&1');
-    echo "\necho \"extension=v8js.so\" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini\n\n";
-    echo shell_exec('echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini 2>&1');
+    foreach (array(
+        'apt-get install libv8-dev libv8-dbg g++ cpp',
+        'printf "\n" | pecl install -f v8js',
+        'echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini',
+    ) as $command) {
+        echo "\n$command\n\n";
+        echo shell_exec("$command 2>&1") . "\n";
+    }
 }
