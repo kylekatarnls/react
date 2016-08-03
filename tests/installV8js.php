8577e3a2c9fd23945fe572b8079204ccb3230e3f
Kyle <kylekatarnls@gmail.com>
2016-08-03 09:25:24 +0200

    Try older V8Js engine

--- tests/installV8js.php
+++ tests/installV8js.php
@@ -2,7 +2,7 @@
 
 if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
     foreach (array(
-        'printf "\n" | pecl install -f v8js',
+        'printf "\n" | pecl install -f v8js-0.6.4',
         'echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini',
     ) as $command) {
         echo "\n$command\n\n";
