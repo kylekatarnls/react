20fd8a20e4915607deab43749059a84dcb815c04
KyleKatarn <kylekatarnls@gmail.com>
2017-05-12 17:42:17 +0200

    Fix PHP 5.3 compatibility

--- tests/installV8js.php
+++ tests/installV8js.php
@@ -2,12 +2,13 @@
 
 if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
     foreach (array(
+        'sudo apt-get install libv8-dev libv8-dbg g++ cpp',
         'git clone https://github.com/phpv8/v8js.git',
         'cd v8js',
         'make -f Makefile.travis before_install',
         'make -f Makefile.travis install',
         'cd ..',
-        'printf "\n" | pecl install -f v8js',
+        'printf "/opt/v8\n" | pecl install -f v8js',
         'echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini',
     ) as $command) {
         echo "\n$command\n\n";
