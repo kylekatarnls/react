2830ed26c269b0d7986e924fe22b354ba49faf8e
kylekatarnls <kylekatarnls@gmail.com>
2017-05-12 18:06:13 +0200

    Customize v8 path install

--- tests/installV8js.php
+++ tests/installV8js.php
@@ -5,10 +5,12 @@ if (version_compare(PHP_VERSION, '7.0.0') >= 0) {
         'sudo apt-get install libv8-dev libv8-dbg g++ cpp',
         'git clone https://github.com/phpv8/v8js.git',
         'cd v8js',
-        'make -f Makefile.travis before_install',
-        'make -f Makefile.travis install',
-        'cd ..',
-        'printf "/usr/lib/v8\n" | pecl install -f v8js',
+        'phpize',
+        './configure --with-v8js=/opt/v8',
+        'make',
+        'make test',
+        'sudo make install',
+        'printf "/opt/v8\n" | pecl install -f v8js',
         'echo "extension=v8js.so" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini',
     ) as $command) {
         echo "\n$command\n\n";
